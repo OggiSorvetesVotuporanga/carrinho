56734675dde8aec8c30d920436aff66ccb1bbdf9
OggiSorvetesVotuporanga <guilherme.b.stella@gmail.com>
2025-03-09 13:53:42 -0300

    script.js

--- script.js
+++ script.js
@@ -362,7 +362,7 @@ function updateTotal() {
     boll.style = "display:none;";
   }
 
-  if (total >= 200 && !alertShown) {
+  if (total >= 300 && !alertShown) {
     alertaConteiner.style = "display: flex";
     alertShown = true; 
   }
