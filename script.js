30fb41b462a7a6f2ae481f5d444ea9a510a52eaa
OggiSorvetesVotuporanga <guilherme.b.stella@gmail.com>
2025-03-09 13:06:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,509 @@
+// NUMERO DE TELEFONE DAS OGGI
+// -------------------------------------
+const numeroWhatsApp = "5517991493839"; //-------------
+// -------------------------------------
+
+const produtos = {
+  melancia: {
+    valor: 1.5,
+    nome: "Festa Melancia",
+    und: 0,
+  },
+  frutasVermelhas: {
+    valor: 1.5,
+    nome: "Festa Frutas Vermelhas",
+    und: 0,
+  },
+  achocolatado: {
+    valor: 1.75,
+    nome: "Achocolatado",
+    und: 0,
+  },
+  chocolateBranco: {
+    valor: 1.75,
+    nome: "Chocolate Branco",
+    und: 0,
+  },
+  iogurte: {
+    valor: 1.75,
+    nome: "Iorgute",
+    und: 0,
+  },
+  leiteCondensado: {
+    valor: 1.75,
+    nome: "Leite Condesado",
+    und: 0,
+  },
+  banana: {
+    valor: 1.75,
+    nome: "Banana",
+    und: 0,
+  },
+  tuttiFrutti: {
+    valor: 1.75,
+    nome: "Tutti Frutti",
+    und: 0,
+  },
+  kiwi: {
+    valor: 1.75,
+    nome: "Kiwi",
+    und: 0,
+  },
+  groselha: {
+    valor: 1.75,
+    nome: "Groselha",
+    und: 0,
+  },
+  pintaLingua: {
+    valor: 1.75,
+    nome: "Pinta Língua",
+    und: 0,
+  },
+  acai: {
+    valor: 2.25,
+    nome: "Açaí",
+    und: 0,
+  },
+  maracuja: {
+    valor: 2.25,
+    nome: "Maracujá",
+    und: 0,
+  },
+  limao: {
+    valor: 2.25,
+    nome: "Limão",
+    und: 0,
+  },
+  abacaxi: {
+    valor: 2.25,
+    nome: "Abacaxi",
+    und: 0,
+  },
+  uva: {
+    valor: 2.25,
+    nome: "Uva",
+    und: 0,
+  },
+  morango: {
+    valor: 2.25,
+    nome: "Morango",
+    und: 0,
+  },
+  doceDeLeite: {
+    valor: 2.5,
+    nome: "Doce de Leite",
+    und: 0,
+  },
+  milho: {
+    valor: 2.5,
+    nome: "Milho",
+    und: 0,
+  },
+  morangoAoLeite: {
+    valor: 2.5,
+    nome: "Morango ao Leite",
+    und: 0,
+  },
+  cocoQueimado: {
+    valor: 2.5,
+    nome: "Coco Queimado",
+    und: 0,
+  },
+  coco: {
+    valor: 2.5,
+    nome: "Coco",
+    und: 0,
+  },
+  amendoim: {
+    valor: 2.5,
+    nome: "Amendoim",
+    und: 0,
+  },
+  panettone: {
+    valor: 2.75,
+    nome: "Panettone",
+    und: 0,
+  },
+  tortaDeLimao: {
+    valor: 2.75,
+    nome: "Torta de Limão",
+    und: 0,
+  },
+  DfrutasVermelhas: {
+    valor: 2.75,
+    nome: "Iorgute com frutas Vermelhas",
+    und: 0,
+  },
+  queijoComGoiabada: {
+    valor: 2.75,
+    nome: "Queijo com Goiabada",
+    und: 0,
+  },
+  mousseDeMaracuja: {
+    valor: 2.75,
+    nome: "Mousse de Maracujá",
+    und: 0,
+  },
+  chocolate: {
+    valor: 2.75,
+    nome: "Chocolate",
+    und: 0,
+  },
+  beijinho: {
+    valor: 2.75,
+    nome: "Beijinho",
+    und: 0,
+  },
+  cookies: {
+    valor: 2.75,
+    nome: "Cookies",
+    und: 0,
+  },
+  abacaxiAoVinho: {
+    valor: 2.75,
+    nome: "Abacaxi ao Vinho",
+    und: 0,
+  },
+  pistache: {
+    valor: 3.99,
+    nome: "Pistache",
+    und: 0,
+  },
+  pudim: {
+    valor: 3.99,
+    nome: "Pudim",
+    und: 0,
+  },
+  cremeDeAvela: {
+    valor: 3.99,
+    nome: "Creme de Avelã",
+    und: 0,
+  },
+  bombomNoPalito: {
+    valor: 4.0,
+    nome: "Bombom no Palito",
+    und: 0,
+  },
+  brigadeiro: {
+    valor: 4.0,
+    nome: "Brigadeiro",
+    und: 0,
+  },
+  uvaL: {
+    valor: 4.0,
+    nome: "Leve Uva",
+    und: 0,
+  },
+  acaiL: {
+    valor: 4.0,
+    nome: "Leve Açai",
+    und: 0,
+  },
+  cocoL: {
+    valor: 4.0,
+    nome: "Leve Coco",
+    und: 0,
+  },
+  milhoL: {
+    valor: 4.0,
+    nome: "Leve Milho",
+    und: 0,
+  },
+  chocolateL: {
+    valor: 4.0,
+    nome: "Leve Chocolate",
+    und: 0,
+  },
+  limaoL: {
+    valor: 4.0,
+    nome: "Leve Limão",
+    und: 0,
+  },
+  amendoimL: {
+    valor: 4.0,
+    nome: "Leve Amendoim",
+    und: 0,
+  },
+  chocoMenta: {
+    valor: 5.99,
+    nome: "Sensa Choco Menta",
+    und: 0,
+  },
+  meioAmargo: {
+    valor: 5.99,
+    nome: "Sensa Meio Amargo",
+    und: 0,
+  },
+  classico: {
+    valor: 5.99,
+    nome: "Sensa Clássico",
+    und: 0,
+  },
+  SdoceDeLeite: {
+    valor: 5.99,
+    nome: "Sensa Doce de Leite",
+    und: 0,
+  },
+  SchocolateBranco: {
+    valor: 5.99,
+    nome: "Sensa Chocolate Branco",
+    und: 0,
+  },
+  chocoAmendoim: {
+    valor: 5.99,
+    nome: "Sensa Choco Amendoim",
+    und: 0,
+  },
+  SfrutasVermelhas: {
+    valor: 5.99,
+    nome: "Sensa Frutas Vermelhas",
+    und: 0,
+  },
+  Sbrigadeiro: {
+    valor: 5.99,
+    nome: "Sensa Brigadeiro",
+    und: 0,
+  },
+};
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+const carrinhoConteiner = document.querySelector(".carrinho-conteiner");
+const open = document.querySelector(".fa-cart-shopping");
+const close = document.querySelector(".closeCarrinho");
+const boll = document.querySelector(".boll");
+
+open.addEventListener("click", function () {
+  carrinhoConteiner.style = "display: flex";
+});
+
+close.addEventListener("click", function () {
+  carrinhoConteiner.style = "display: none";
+});
+openCar = () => {
+  carrinhoConteiner.style = "display: flex";
+  alertaConteiner.style = "display: none";
+};
+const alertaConteiner = document.querySelector(".alertaConteiner");
+const closeAlerta = document.querySelector(".closeAlerta");
+
+closeAlerta.addEventListener("click", function () {
+  alertaConteiner.style = "display: none";
+});
+
+const conteinerProdutos = document.querySelector(".conteiner-produtos");
+const conteinerDadosUser = document.querySelector(".conteinerDadosUser");
+finalizar = () => {
+  carrinhoConteiner.style = "display: none";
+  conteinerDadosUser.style = "display: flex;";
+};
+fecharFinal = () => {
+  conteinerDadosUser.style = "display: none;";
+};
+// Função para atualizar o histórico dos picolés
+function updateHistorico() {
+  conteinerProdutos.innerHTML = "";
+  for (const key in produtos) {
+    const produto = produtos[key];
+    if (produto.und > 0) {
+      const produtosLista = document.createElement("ul");
+      produtosLista.classList.add("produtosLista");
+
+      const quantidadeItem = document.createElement("li");
+      quantidadeItem.textContent = produto.und;
+      produtosLista.appendChild(quantidadeItem);
+
+      const valorItem = document.createElement("li");
+      valorItem.textContent = `R$ ${(produto.valor * produto.und).toFixed(2)}`;
+      produtosLista.appendChild(valorItem);
+
+      const nomeItem = document.createElement("li");
+      nomeItem.textContent = produto.nome;
+      produtosLista.appendChild(nomeItem);
+
+      conteinerProdutos.appendChild(produtosLista);
+    }
+  }
+}
+
+// Processo para aumentar e diminuir as unidades
+function updateQuantity(productKey, change) {
+  const produto = produtos[productKey];
+  if (produto) {
+    produto.und = Math.max(0, produto.und + change); 
+    document.getElementById(`${productKey}-quantity`).textContent = produto.und;
+    updateTotal();
+  }
+}
+let alertShown = false;
+// Processo de soma de produtos e unidades
+function updateTotal() {
+  const total = Object.values(produtos).reduce(
+    (acc, item) => acc + item.valor * item.und,
+    0
+  );
+  document.querySelector(
+    ".valorGeral"
+  ).innerHTML = `Total:<br> <strong>R$ ${total.toFixed(2)}</strong>`;
+
+  const und = Object.values(produtos).reduce((acc, item) => acc + item.und, 0);
+  document.querySelector(
+    ".unidadeGeral"
+  ).innerHTML = `Unidades:<br> <strong>${und}</strong>`;
+
+  document.querySelector(
+    ".TotalR"
+  ).innerHTML = `Total:<br> <strong>R$ ${total.toFixed(2)}</strong>`;
+  document.querySelector(
+    ".undR"
+  ).innerHTML = `Unidades:<br> <strong>${und}</strong>`;
+  if (und > 0) {
+    boll.style = "display:flex;";
+  } else {
+    boll.style = "display:none;";
+  }
+
+  if (total >= 300 && !alertShown) {
+    alertaConteiner.style = "display: flex";
+    alertShown = true; 
+  }
+
+  updateHistorico(); // Atualiza o histórico sempre que o total é atualizado
+}
+updateTotal();
+
+document.getElementById("enviarPedido").addEventListener("click", function () {
+  const nomeUser = document.getElementById("nomeUser").value;
+  const dataReserva = document.getElementById("dataReserva").value;
+
+  if (nomeUser == "" || dataReserva == "") {
+    alert("Preencha todos os campos!");
+  } else {
+    const terminoConteiner = document.querySelector(".terminoConteiner");
+    terminoConteiner.style = "display:flex;";
+
+    const dataFormatada = new Date(dataReserva).toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+    let mensagem = `Nome: ${nomeUser}\nData de Reserva: (${dataFormatada})\n\nProdutos:\n`;
+
+    let totalGeral = 0;
+    let totalUnidades = 0;
+
+    for (const key in produtos) {
+      const produto = produtos[key];
+      if (produto.und > 0) {
+        const totalProduto = produto.valor * produto.und;
+        totalGeral += totalProduto;
+        totalUnidades += produto.und;
+        mensagem += `${produto.nome} *(${produto.und})* \n`;
+      }
+    }
+
+    mensagem += `\nTotal de Unidades: *${totalUnidades}*\nTotal Geral: *R$ ${totalGeral
+      .toFixed(2)
+      .replace(".", ",")}*`;
+
+    const urlWhatsApp = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${encodeURIComponent(
+      mensagem
+    )}`;
+
+    window.open(urlWhatsApp, "_blank");
+  }
+});
+
+// LISTAGEM DE COMBOS
+const sugestaoConteiner = document.querySelector(".sugestao-conteiner");
+openSugestoes = () => {
+  sugestaoConteiner.style = "display:flex;";
+};
+fecharSugestoes = () => {
+  sugestaoConteiner.style = "display:none;";
+};
+
+const exibirCarrinhoEAtualizar = () => {
+  carrinhoConteiner.style = "display: flex";
+  updateHistorico();
+  updateTotal();
+}
+const updateCombo = () =>{
+  document.getElementById('melancia-quantity').innerHTML = produtos.melancia.und;
+  document.getElementById('frutasVermelhas-quantity').innerHTML = produtos.frutasVermelhas.und;
+  document.getElementById('achocolatado-quantity').innerHTML = produtos.achocolatado.und;
+  document.getElementById('chocolateBranco-quantity').innerHTML = produtos.chocolateBranco.und;
+  document.getElementById('iogurte-quantity').innerHTML = produtos.iogurte.und;
+  document.getElementById('leiteCondensado-quantity').innerHTML = produtos.leiteCondensado.und;
+  document.getElementById('banana-quantity').innerHTML = produtos.banana.und;
+  document.getElementById('tuttiFrutti-quantity').innerHTML = produtos.tuttiFrutti.und;
+  document.getElementById('kiwi-quantity').innerHTML = produtos.kiwi.und;
+  document.getElementById('groselha-quantity').innerHTML = produtos.groselha.und;
+  document.getElementById('pintaLingua-quantity').innerHTML = produtos.pintaLingua.und;
+}
+const combo = (key) => {
+switch (key) {
+  case "combo1":
+    produtos.melancia.und = 100;
+    produtos.frutasVermelhas.und = 100;
+    produtos.achocolatado.und = 0;
+    produtos.chocolateBranco.und = 0;
+    produtos.iogurte.und = 0;
+    produtos.leiteCondensado.und = 0;
+    produtos.banana.und = 0;
+    produtos.tuttiFrutti.und = 0;
+    produtos.kiwi.und = 0;
+    produtos.groselha.und = 0;
+    produtos.pintaLingua.und = 0;
+    exibirCarrinhoEAtualizar();
+    updateCombo();
+    break;
+  case "combo2":
+    produtos.melancia.und = 40;
+    produtos.frutasVermelhas.und = 40;
+    produtos.achocolatado.und = 22;
+    produtos.chocolateBranco.und = 21;
+    produtos.iogurte.und = 20;
+    produtos.leiteCondensado.und = 20;
+    produtos.banana.und = 20;
+    produtos.tuttiFrutti.und = 0;
+    produtos.kiwi.und = 0;
+    produtos.groselha.und = 0;
+    produtos.pintaLingua.und = 0;
+    exibirCarrinhoEAtualizar();
+    updateCombo();
+    break;
+  case "combo3":
+    produtos.melancia.und = 0;
+    produtos.frutasVermelhas.und = 0;
+    produtos.achocolatado.und = 35;
+    produtos.chocolateBranco.und = 35;
+    produtos.iogurte.und = 35;
+    produtos.leiteCondensado.und = 34;
+    produtos.banana.und = 33;
+    produtos.tuttiFrutti.und = 0;
+    produtos.kiwi.und = 0;
+    produtos.groselha.und = 0;
+    produtos.pintaLingua.und = 0;
+    exibirCarrinhoEAtualizar();
+    updateCombo();
+    break;
+  case "combo4":
+    produtos.melancia.und = 25;
+    produtos.frutasVermelhas.und = 25;
+    produtos.achocolatado.und = 0;
+    produtos.chocolateBranco.und = 0;
+    produtos.iogurte.und = 0;
+    produtos.leiteCondensado.und = 0;
+    produtos.banana.und = 0;
+    produtos.tuttiFrutti.und = 30;
+    produtos.kiwi.und = 30;
+    produtos.groselha.und = 35;
+    produtos.pintaLingua.und = 35;
+    exibirCarrinhoEAtualizar();
+    updateCombo();
+    break;
+  default:
+    alert("Sugestão não encontrada!");
+    break;
+}
+};
